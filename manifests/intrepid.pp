ec178e0b16d016c1a9aaa1844c9ce82e59e90229
Sanjaya Perera <sanjaya.perera@intrepidtravel.com>
2015-02-20 10:18:36 +1100

    File Modifications

--- manifests/intrepid.pp
+++ manifests/intrepid.pp
@@ -1,23 +1,35 @@
 node 'web.intrepidtravel.com.dev.local'{
+	class { selinux:
+  		mode => 'disabled'
+	}
 	class { '::ntp':
   		servers => [ '10.2.1.2', '10.1.1.2' ],
 	}
-	include pupi
+	class { 'motd':
+  		template => 'motd.intrepid.web.dev.local',
+	} 
 	include repos
 	include nginx
+	include curl
 	include php56
-	include peak-tools
+	include git
+	include bash
+	include memcached	
 	
 }
 
 node 'db.intrepidtravel.com.dev.local'{	
+	include bash
+	class { selinux:
+  		mode => 'disabled'
+	}
 	class { '::ntp':
   		servers => [ '10.2.1.2', '10.1.1.2' ],
 	}
-
+	class { 'motd':
+  		template => 'motd.intrepid.db.dev.local',
+	}
 	class { "mysql":
  		source => "/vagrant/files/my.cnf",
     	}
-	include pupi
-	include peak-tools
 }
